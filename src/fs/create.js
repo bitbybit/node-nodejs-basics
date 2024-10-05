782fe983dcbef89c285234f236e687ca196506af
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-10-05 21:18:03 +0200

    refactor: use code instead of errno to check an error in fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,5 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
-import { constants } from 'node:os'
 
 const filePath = `${import.meta.dirname}/files/fresh.txt`
 const fileContent = 'I am fresh and young'
@@ -17,7 +16,7 @@ const create = async () => {
 
       await fs.writeFile(pathToFileAbsolute, fileContent, { flag: 'wx' })
     } catch (e) {
-      const fileExists = e?.errno === -constants.errno.EEXIST
+      const fileExists = e?.code === 'EEXIST'
 
       if (fileExists) {
         throw new Error(messageFileExists)
