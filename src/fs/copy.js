57816ad0b96d22bc8d1bdce63c45d21051466d71
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-10-05 21:18:45 +0200

    feat: implement fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
-const copy = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs/promises'
+import path from 'node:path'
 
-await copy();
+const dirPath = `${import.meta.dirname}/files`
+const dirCopyPath = `${import.meta.dirname}/files_copy`
+const messageFailed = 'FS operation failed'
+
+/**
+ * @description Copies folder `files` files with all its content into folder `files_copy` at the same level (if `files` folder doesn't exists or `files_copy` has already been created `Error` with message `FS operation failed` must be thrown)
+ * @returns {Promise<void>}
+ * @throws {Error}
+ */
+const copy = async () => {
+    try {
+      const pathToDirAbsolute = path.resolve(dirPath)
+      const pathToDirCopyAbsolute = path.resolve(dirCopyPath)
+
+      await fs.cp(pathToDirAbsolute, pathToDirCopyAbsolute, {
+        errorOnExist: true,
+        force: false,
+        recursive: true
+      })
+    } catch (e) {
+      const isMissingDir = e?.code === 'ENOENT'
+      const dirCopyExists = e?.code === 'ERR_FS_CP_EEXIST'
+
+      if (isMissingDir || dirCopyExists) {
+        throw new Error(messageFailed)
+      } else {
+        throw e
+      }
+    }
+}
+
+await copy()
