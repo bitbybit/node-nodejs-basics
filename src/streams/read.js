96b7b6aec6db3e79cfd3abb7f537211b0c6c47f4
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-10-06 16:54:28 +0200

    feat: implement streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,36 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import os from 'node:os'
 
-await read();
+const filePath = `${import.meta.dirname}/files/fileToRead.txt`
+const filePathAbsolute = path.resolve(filePath)
+
+/**
+ * @description Reads file `fileToRead.txt` content using Readable Stream and prints it's content into `process.stdout`
+ * @returns {Promise<void>}
+ */
+const read = async () => {
+  const file = await fs.open(filePathAbsolute, 'r')
+
+  const stream = file.createReadStream()
+
+  return new Promise((resolve, reject) => {
+    const chunks = []
+
+    stream.on('error', error => reject(error))
+
+    stream.on('data', (chunk) => {
+      chunks.push(chunk)
+    });
+
+    stream.on('end', () => {
+      const content = chunks.join()
+
+      process.stdout.write(`${content}${os.EOL}`)
+
+      resolve()
+    })
+  })
+}
+
+await read()
