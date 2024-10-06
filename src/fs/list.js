5bbe1503fc201c28e7ac462cca86e5c0fbef0b47
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-10-06 11:35:13 +0200

    feat: implement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,31 @@
-const list = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs/promises'
+import path from 'node:path'
 
-await list();
+const dirPath = `${import.meta.dirname}/files`
+const dirPathAbsolute = path.resolve(dirPath)
+const messageDirMissing = 'FS operation failed'
+
+/**
+ * @description Prints all array of filenames from `files` folder into console (if `files` folder doesn't exists `Error` with message `FS operation failed` must be thrown)
+ * @returns {Promise<void>}
+ * @throws {Error}
+ */
+const list = async () => {
+  try {
+    const files = await fs.readdir(dirPathAbsolute)
+
+    for (const file of files) {
+      console.log(file)
+    }
+  } catch (e) {
+    const isMissingDir = e?.code === 'ENOENT'
+
+    if (isMissingDir) {
+      throw new Error(messageDirMissing)
+    } else {
+      throw e
+    }
+  }
+}
+
+await list()
