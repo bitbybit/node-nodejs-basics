a0ff94a787ab420fd65473285d29f5f90988ce30
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-10-06 13:08:27 +0200

    feat: implement cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,22 @@
-const parseEnv = () => {
-    // Write your code here 
-};
+const prefix = 'RSS_'
+const separatorOfValues = '='
+const separatorOfItems = '; '
 
-parseEnv();
+/**
+ * @description Parses environment variables with prefix `RSS_` and prints them to the console in the format `RSS_name1=value1; RSS_name2=value2`
+ */
+const parseEnv = () => {
+  const envs = Object.entries(process.env)
+
+  const envsFiltered = envs.filter(([key]) => key.startsWith(prefix))
+
+  const envsMapped = envsFiltered.map(
+    ([key, value]) => `${key}${separatorOfValues}${value}`
+  )
+
+  const envsFormatted = envsMapped.join(separatorOfItems)
+
+  console.log(envsFormatted)
+}
+
+parseEnv()
