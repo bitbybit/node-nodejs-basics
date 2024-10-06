1a89332e8d8bef9eb4cca912c7d6aa03d0ad59f5
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-10-06 11:17:54 +0200

    feat: implement fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,27 @@
-const remove = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs/promises'
+import path from 'node:path'
 
-await remove();
+const filePath = `${import.meta.dirname}/files/fileToRemove.txt`
+const filePathAbsolute = path.resolve(filePath)
+const messageFileMissing = 'FS operation failed'
+
+/**
+ * @description Deletes file `fileToRemove.txt` (if there's no file `fileToRemove.txt` `Error` with message `FS operation failed` must be thrown)
+ * @returns {Promise<void>}
+ * @throws {Error}
+ */
+const remove = async () => {
+  try {
+    await fs.rm(filePathAbsolute)
+  } catch (e) {
+    const isMissingFile = e?.code === 'ENOENT'
+
+    if (isMissingFile) {
+      throw new Error(messageFileMissing)
+    } else {
+      throw e
+    }
+  }
+}
+
+await remove()
