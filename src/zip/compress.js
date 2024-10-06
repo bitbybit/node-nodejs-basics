02ba34587ea54012fd7c5f51d7dc543d62f99875
bitbybit <1628191+bitbybit@users.noreply.github.com>
2024-10-06 18:02:52 +0200

    feat: implement zip:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
-const compress = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import zlib from 'node:zlib'
 
-await compress();
+const filePath = `${import.meta.dirname}/files/fileToCompress.txt`
+const fileArchivePath = `${import.meta.dirname}/files/archive.gz`
+const filePathAbsolute = path.resolve(filePath)
+const fileArchivePathAbsolute = path.resolve(fileArchivePath)
+
+/**
+ * @description Compresses file `fileToCompress.txt` to `archive.gz` using `zlib` and Streams API
+ * @returns {Promise<void>}
+ */
+const compress = async () => {
+  const file = await fs.open(filePathAbsolute, 'r')
+  const archive = await fs.open(fileArchivePathAbsolute, 'w')
+
+  const stream = file.createReadStream()
+  const streamArchive = archive.createWriteStream()
+
+  const archiveFile = zlib.createGzip()
+
+  stream.pipe(archiveFile).pipe(streamArchive)
+}
+
+await compress()
